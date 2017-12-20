b1a679f19c2d30d2175ae8b7463c04ec74e14890
igorsnezko <i.s.sergeevich@yandex.ru>
2017-12-20 19:16:54 +0300

    добавлен функционал кошельков и платежных систем

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,9 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-//        $this->call(AdminUserTableSeeder::class);
-//	    $this->call(RoleTableSeeder::class);
-//	    $this->call(UserRoleTableSeeder::class);
+        $this->call(AdminUserTableSeeder::class);
+	    $this->call(RoleTableSeeder::class);
+	    $this->call(UserRoleTableSeeder::class);
 	    $this->call(PaymentSystemTableSeeder::class);
     }
 }
