04eaf7e7dcf5cdd942ca113f285d77dd15613d9a
igorsnezko <i.s.sergeevich@yandex.ru>
2018-03-12 15:50:36 +0300

    регистрация, активация

--- www/config/app.php
+++ www/config/app.php
@@ -53,6 +53,7 @@ return [
     */
 
     'url' => env('APP_URL', 'http://localhost'),
+    'website_url' => env('WEBSITE_URL', 'http://localhost'),
 
     /*
     |--------------------------------------------------------------------------
