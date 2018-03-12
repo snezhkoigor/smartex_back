b3cd44670ffedfc0391b063ea271a744a6df43a2
igorsnezko <i.s.sergeevich@yandex.ru>
2018-03-12 15:29:54 +0300

    регистрация, активация

--- www/resources/views/emails/user/registration/plain.blade.php
+++ www/resources/views/emails/user/registration/plain.blade.php
@@ -1,22 +1,16 @@
 @extends('emails.layouts.html.index')
 
 @section('content')
-	<p>
-		Dear client{{$user->name ? ', ' . $user->name : ''}}!
-	</p>
-	<p>
-		We are happy to confirm your registration on {{config('app.name')}}.
-	</p>
-	<p>
-		E-mail: {{$user->email}}<br/>
-		Password: {{$password}}
-	</p>
-	<p>
-		Looking forward the future cooperation.<br/>
-		If you have any questions, do not hesitate to contact us on our contact email through the online support chat on our website.
-	</p>
-	<p>
-		To complete the registration please follow the link below and confirm your e-mail address:<br/>
-		<a href='{{config('app.website_url') . '/user/activation/' . md5($user->email)}}'>{{config('app.website_url') . '/user/activation/' . md5($user->email)}}</a>
-	</p>
+Dear client{{$user->name ? ', ' . $user->name : ''}}!
+
+We are happy to confirm your registration on {{config('app.name')}}.
+
+E-mail: {{$user->email}}<br/>
+Password: {{$password}}
+
+Looking forward the future cooperation.
+If you have any questions, do not hesitate to contact us on our contact email through the online support chat on our website.
+
+To complete the registration please follow the link below and confirm your e-mail address:
+{{config('app.website_url') . '/user/activation/' . md5($user->email)}}
 @stop
