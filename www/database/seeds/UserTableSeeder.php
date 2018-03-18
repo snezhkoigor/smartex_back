d959185b744b8d51a4c78b83a2b0f1309a366446
igorsnezko <i.s.sergeevich@yandex.ru>
2018-03-18 21:13:15 +0300

    вывод списка новостей

---
+++ www/database/seeds/UserTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->delete();
+        $user = new \App\Models\User();
+        $user->email = 'admin@cryptocoinofcina.com';
+        $user->password = \Illuminate\Support\Facades\Hash::make('123admin123');
+        
+        $user->save();
+    }
+}
