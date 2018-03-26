f1484638d92bbcff6ef255116c7ef16866e9f109
igorsnezko <i.s.sergeevich@yandex.ru>
2018-03-26 16:52:08 +0300

    добавление вывода списка для пользователей списка коммиссий

--- www/app/Http/Controllers/Api/CommissionController.php
+++ www/app/Http/Controllers/Api/CommissionController.php
@@ -83,7 +83,7 @@ class CommissionController extends Controller
 	    $commissions = CommissionRepository::getCommissions($filters, $relations, ['*'], $search_string, $limit, $offset);
 
 	    return fractal($commissions, new CommissionTransformer())
-		    ->parseIncludes(['paymentSystem', 'wallet'])
+		    ->parseIncludes(['paymentSystem', 'wallet.paymentSystem'])
 		    ->respond();
 	}
 
