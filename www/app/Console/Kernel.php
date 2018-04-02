89163ed14f8e1e8985cb357f781b49fda38a84ef
igorsnezko <i.s.sergeevich@yandex.ru>
2018-04-02 15:26:39 +0300

    добавление парсера курсов валют

--- www/app/Console/Kernel.php
+++ www/app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
 	    $schedule->command('activitylog:clean')->daily();
-	    $schedule->command('courses:parse')->everyFiveMinutes();
+	    $schedule->command('courses:parse')->hourly();
     }
 
     /**
