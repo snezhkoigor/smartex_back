fd3a1367d41c2374aaaf1a4f9b666f745177b8c0
igorsnezko <i.s.sergeevich@yandex.ru>
2018-04-04 11:40:23 +0300

    добавление создания транзакции на обмен

--- www/database/seeds/PaymentSystemTableSeeder.php
+++ www/database/seeds/PaymentSystemTableSeeder.php
@@ -21,6 +21,7 @@ class PaymentSystemTableSeeder extends Seeder
 	    $payment_system->name = 'Bank';
 	    $payment_system->code = 'bank';
 	    $payment_system->logo = null;
+	    $payment_system->fields = 'balance';
 	    $payment_system->is_account_multi_line = true;
 	    $payment_system->save();
 
@@ -28,35 +29,35 @@ class PaymentSystemTableSeeder extends Seeder
 	    $payment_system->name = 'Perfect Money';
 	    $payment_system->code = 'pm';
 	    $payment_system->logo = 'perfect-money.png';
-	    $payment_system->fields = 'secret,user,password';
+	    $payment_system->fields = 'secret,user,password,balance';
 	    $payment_system->save();
 
 	    $payment_system = new PaymentSystem();
 	    $payment_system->name = 'Payeer';
 	    $payment_system->code = 'payeer';
 	    $payment_system->logo = 'payeer.png';
-	    $payment_system->fields = 'secret,user,password';
+	    $payment_system->fields = 'secret,user,password,balance';
 	    $payment_system->save();
 
 	    $payment_system = new PaymentSystem();
 	    $payment_system->name = 'ADV';
 	    $payment_system->code = 'adv';
 	    $payment_system->logo = 'adv-cash.png';
-	    $payment_system->fields = 'secret,user,password,id_payee,adv_sci';
+	    $payment_system->fields = 'secret,user,password,balance,id_payee,adv_sci';
 	    $payment_system->save();
 
 	    $payment_system = new PaymentSystem();
 	    $payment_system->name = 'BTC';
 	    $payment_system->code = 'btc';
 	    $payment_system->logo = 'bitcoin.png';
-	    $payment_system->fields = 'secret,user,password';
+	    $payment_system->fields = 'secret,user,password,balance';
 	    $payment_system->save();
 
 	    $payment_system = new PaymentSystem();
 	    $payment_system->name = 'ETH';
 	    $payment_system->code = 'eth';
 	    $payment_system->logo = 'etherium.png';
-	    $payment_system->fields = 'secret,user,password';
+	    $payment_system->fields = 'secret,user,password,balance';
 	    $payment_system->save();
 
 	    $payment_system = new PaymentSystem();
@@ -64,7 +65,7 @@ class PaymentSystemTableSeeder extends Seeder
 	    $payment_system->code = 'eth';
 	    $payment_system->logo = 'dash.png';
 	    $payment_system->active = 0;
-	    $payment_system->fields = 'secret,user,password';
+	    $payment_system->fields = 'secret,user,password,balance';
 	    $payment_system->save();
 
 	    $payment_system = new PaymentSystem();
@@ -72,7 +73,7 @@ class PaymentSystemTableSeeder extends Seeder
 	    $payment_system->code = 'zcash';
 	    $payment_system->logo = 'zcash.png';
 	    $payment_system->active = 0;
-	    $payment_system->fields = 'secret,user,password';
+	    $payment_system->fields = 'secret,user,password,balance';
 	    $payment_system->save();
 
 	    $payment_system = new PaymentSystem();
@@ -80,7 +81,7 @@ class PaymentSystemTableSeeder extends Seeder
 	    $payment_system->code = 'ripple';
 	    $payment_system->logo = 'ripple.png';
 	    $payment_system->active = 0;
-	    $payment_system->fields = 'secret,user,password';
+	    $payment_system->fields = 'secret,user,password,balance';
 	    $payment_system->save();
 
 	    $payment_system = new PaymentSystem();
@@ -88,7 +89,7 @@ class PaymentSystemTableSeeder extends Seeder
 	    $payment_system->code = 'monero';
 	    $payment_system->logo = 'monero.png';
 	    $payment_system->active = 0;
-	    $payment_system->fields = 'secret,user,password';
+	    $payment_system->fields = 'secret,user,password,balance';
 	    $payment_system->save();
 
 	    $payment_system = new PaymentSystem();
@@ -96,7 +97,7 @@ class PaymentSystemTableSeeder extends Seeder
 	    $payment_system->code = 'lcoin';
 	    $payment_system->logo = 'litecoin.png';
 	    $payment_system->active = 0;
-	    $payment_system->fields = 'secret,user,password';
+	    $payment_system->fields = 'secret,user,password,balance';
 	    $payment_system->save();
 
 	    Model::reguard();
