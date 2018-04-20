484f0f22edf5d462e76e0b3ac03152bfe8fc3441
igorsnezko <i.s.sergeevich@yandex.ru>
2018-04-20 11:17:15 +0300

    обработка pm после прихода платежа

--- www/app/Console/Kernel.php
+++ www/app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
 	    $schedule->command('activitylog:clean')->daily();
-	    $schedule->command('courses:parse')->everyMinute();
+	    $schedule->command('courses:parse')->hourly();
     }
 
     /**
